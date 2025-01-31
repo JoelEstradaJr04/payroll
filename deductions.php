00d19e1adb4a616140065fdfe4d16f2ace3e7331
Estrada Jr., Joel R. <joelestrada021604@gmail.com>
2025-02-01 06:29:26 +0800

    Deductions Read

--- deductions.php
+++ deductions.php
@@ -1,97 +1,96 @@
-<?php include('db_connect.php');?>
+<?php include('db_connect.php'); ?>
 
 <div class="container-fluid">
-	
-	<div class="col-lg-12">
-		<div class="row">
-			<!-- FORM Panel -->
-			<div class="col-md-4">
-			<form action="" id="manage-deductions">
-				<div class="card">
-					<div class="card-header">
-						  Deductions Form
-				  	</div>
-					<div class="card-body">
-							<input type="hidden" name="id">
-							<div class="form-group">
-								<label class="control-label">Deduction Name</label>
-								<textarea name="deduction" id="" cols="30" rows="2" class="form-control" required></textarea>
-							</div>
-							<div class="form-group">
-								<label class="control-label">Description</label>
-								<textarea name="description" id="" cols="30" rows="2" class="form-control" required></textarea>
-							</div>
-							
-							
-							
-					</div>
-							
-					<div class="card-footer">
-						<div class="row">
-							<div class="col-md-12">
-								<button class="btn btn-sm btn-primary col-sm-3 offset-md-3"> Save</button>
-								<button class="btn btn-sm btn-default col-sm-3" type="button" onclick="_reset()"> Cancel</button>
-							</div>
-						</div>
-					</div>
-				</div>
-			</form>
-			</div>
-			<!-- FORM Panel -->
+    <div class="col-lg-12">
+        <div class="row">
+            <div class="col-md-4">
+                <form action="" id="manage-deductions">
+                    <div class="card">
+                        <div class="card-header">
+                            Deductions Form
+                        </div>
+                        <div class="card-body">
+                            <input type="hidden" name="id">
+                            <div class="form-group">
+                                <label class="control-label">Deduction Name</label>
+                                <textarea name="deduction" id="" cols="30" rows="2" class="form-control" required></textarea>
+                            </div>
+                            <div class="form-group">
+                                <label class="control-label">Description</label>
+                                <textarea name="description" id="" cols="30" rows="2" class="form-control" required></textarea>
+                            </div>
+                        </div>
+                        <div class="card-footer">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <button class="btn btn-sm btn-primary col-sm-3 offset-md-3">Save</button>
+                                    <button class="btn btn-sm btn-default col-sm-3" type="button" onclick="_reset()">Cancel</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
 
-			<!-- Table Panel -->
-			<div class="col-md-8">
-				<div class="card">
-					<div class="card-body">
-						<table class="table table-bordered table-hover">
-							<thead>
-								<tr>
-									<th class="text-center">#</th>
-									<th class="text-center">Deduction Information</th>
-									<th class="text-center">Action</th>
-								</tr>
-							</thead>
-							<tbody>
-								<?php 
-								$i = 1;
-								$deductions = $conn->query("SELECT * FROM deductions order by id asc");
-								while($row=$deductions->fetch_assoc()):
-								?>
-								<tr>
-									<td class="text-center"><?php echo $i++ ?></td>
-									
-									<td class="">
-										 <p>Name: <b><?php echo $row['deduction'] ?></b></p>
-										 <p class="truncate"><small>Description: <b><?php echo $row['description'] ?></b></small></p>
-									</td>
-									<td class="text-center">
-										<button class="btn btn-sm btn-primary edit_deductions" type="button" data-id="<?php echo $row['id'] ?>" data-deduction="<?php echo $row['deduction'] ?>" data-description="<?php echo $row['description'] ?>" >Edit</button>
-										<button class="btn btn-sm btn-danger delete_deductions" type="button" data-id="<?php echo $row['id'] ?>">Delete</button>
-									</td>
-								</tr>
-								<?php endwhile; ?>
-							</tbody>
-						</table>
-					</div>
-				</div>
-			</div>
-			<!-- Table Panel -->
-		</div>
-	</div>	
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-body">
+                        <table class="table table-bordered table-hover">
+                            <thead>
+                                <tr>
+                                    <th class="text-center">#</th>
+                                    <th class="text-center">Deduction Information</th>
+                                    <th class="text-center">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                $sql = "SELECT * FROM deductions ORDER BY id ASC"; // SQL Server query
+                                $stmt = sqlsrv_query($conn, $sql);
 
+                                if ($stmt === false) {
+                                    die(print_r(sqlsrv_errors(), true));
+                                }
+
+                                $i = 1;
+                                while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)):
+                                    ?>
+                                    <tr>
+                                        <td class="text-center"><?php echo $i++; ?></td>
+                                        <td class="">
+                                            <p>Name: <b><?php echo $row['deduction']; ?></b></p>
+                                            <p class="truncate"><small>Description: <b><?php echo $row['description']; ?></b></small></p>
+                                        </td>
+                                        <td class="text-center">
+                                            <button class="btn btn-sm btn-primary edit_deductions" type="button" data-id="<?php echo $row['id']; ?>" data-deduction="<?php echo $row['deduction']; ?>" data-description="<?php echo $row['description']; ?>">Edit</button>
+                                            <button class="btn btn-sm btn-danger delete_deductions" type="button" data-id="<?php echo $row['id']; ?>">Delete</button>
+                                        </td>
+                                    </tr>
+                                <?php endwhile;
+                                sqlsrv_free_stmt($stmt); // Free statement resource
+                                ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+
 <style>
-	
-	td{
-		vertical-align: middle !important;
-	}
-	td p{
-		margin: unset
-	}
-	img{
-		max-width:100px;
-		max-height:150px;
-	}
+    td {
+        vertical-align: middle !important;
+    }
+
+    td p {
+        margin: unset
+    }
+
+    img {
+        max-width: 100px;
+        max-height: 150px;
+    }
 </style>
 <script>
 	function _reset(){
